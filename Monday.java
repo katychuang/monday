284bf410bad6fd3a02f73e25349cbb6333d511ad
Dr. Kat <katychuang@users.noreply.github.com>
2020-08-31 10:43:14 -0400

    Add comments :pencil2:
    blah blah blah

--- Monday.java
+++ Monday.java
@@ -1,4 +1,7 @@
+// Sample file to show hello world and I'll write more later to describe this
 public class Monday {
+  
+  // main method required in java
   public static void main(String[] args){
     System.out.println("Hello World");
     
